5945af3f0c46f36dc8a65b56d54df18abb80fc8a
Dave Raney <raneyda.spam@gmail.com>
2017-12-31 12:59:46 -0600

    Updated ceelo

--- dice/ceelo.py
+++ dice/ceelo.py
@@ -4,19 +4,19 @@
 #
 '''
     Rules to Cee-Lo (no banker)
-    3 Dice
+
+    Each player rolls 3 Dice
+
     Roll 4,5,6 Automatic Win
     Roll 1,2,3 Automatic Lose
     Roll a pair and the odd dice is the kicker
      - Kicker value of 1 automatic lose
      - Kicker value of 6 automatic win
 
-    A set point is required and is implied if there is not
-    an automatic win / lose.  The set point is the kicker
-    of 2,3,4,5.
+    A player rolls until they set a point.  The set point is the kicker
+    of 2,3,4,5.  If no doubles are rolled player re-rolls.
 
-    If no doubles are rolled a set point isn't set and the
-    first player re-rolls.
+    Trips (3 matching dice) are not treated differntly than doubles.
 
 '''
 
@@ -26,7 +26,29 @@
 import random
 
 #
+#   Set constants
+#
+game_version = .1
+
+#
+#   Define variables
+#
+num_players = 4
+push_round = 0
+betting_pool = 0
+current_round = 0
+player_points = []
+player_banks = []
+player_bet = []
+active_players = []
+
+
+#
 #   Functions
+#
+
+#
+#   Roll a 6 sided die one time
 #
 def roll_dice():
     dice_value = 0
@@ -53,6 +75,9 @@ def roll_dice():
 
     return dice_value
 
+#
+#   Roll 3 dice, sort, and return the value as 1-6
+#
 def shoot_dice(player_number):
     dice = [roll_dice(), roll_dice(), roll_dice()]
     dice.sort()
@@ -74,11 +99,21 @@ def shoot_dice(player_number):
     # no setpoint yet
     return 0
 
+#
+#   Print the players banks
+#
 def print_banks(number_of_players):
     #   Print current banks
+    total_bank = 0
     for i in range(number_of_players):
         print('Current bank of player {:1d}: {:1d}'.format(i+1, player_banks[i]))
+        total_bank += player_banks[i]
+    #print('Betting pool: {:1d}.  Total of banks: {:1d}'.format(betting_pool,total_bank))
+    return
 
+#
+#   Place a $10 bet for each remaining player (bank > 0)
+#
 def place_bets(players):
     #   Print current banks
     for player in players:
@@ -86,17 +121,28 @@ def place_bets(players):
             player_bet[player] = 10
             player_banks[player] = player_banks[player] - 10
             player_points[player] = 0
+    return
 
+#
+#   A testing routine to determine what point each player had
+#
 def print_setpoints(players):
     for player in players:
         if (player_points[player] != 0):
             print('Player {:1d} setpoint is: {:1d}'.format(player+1, player_points[player]))
+    return
 
+#
+#   Plays a single round of Cee-Lo and determines the winner or pushes to
+#   the next round with only those players that push
+#
 def play_round(players):
-    # Define Constants
+    #
+    # Define local constants
+    #
     setpoint = 0
     setpoint_player = 0
-    betting_pool = 0
+    global betting_pool
     point = 0
     push = 0
 
@@ -123,6 +169,7 @@ def play_round(players):
                     betting_pool = betting_pool + player_bet[l]
                     player_bet[l] = 0
             player_banks[player] = player_banks[player] + betting_pool
+            betting_pool = 0
             print('Player {:1d} automatic win!'.format(player+1))
             push = 0
             break
@@ -142,19 +189,31 @@ def play_round(players):
             else:
                 print('Player {:1d} pushes! ({:1d})'.format(player+1,point))
                 push = 1
+
+    #
+    #   Payout the winner
+    #
     if (push == 0) and (point != 6):
         for l in range(num_players):
             if player_bet[l] > 0:
                 betting_pool = betting_pool + player_bet[l]
                 player_bet[l] = 0
         player_banks[setpoint_player] = player_banks[setpoint_player] + betting_pool
+        betting_pool = 0
+        print('Player {:1d} wins! ({:1d})'.format(setpoint_player+1,player_points[setpoint_player]))
+    return push
 
-def find_active_players():
+#
+#   Determines the number of remainin players as a list
+#   remaining players are those with a bank > 0
+#   Shuffles the list so the first player is always different
+#
+def find_active_players(fa_push=0):
     # Reset current active players
-    print('FA - Push value: {:1d}'.format(push))
-        # Determine if previous round was a push
-    if push == 1:
-        print('Finding active pushed players')
+
+    # Determine if previous round was a push
+    if fa_push == 1:
+        #print('Finding active pushed players')
         push_players = max(player_points)
         for i in range(num_players):
             if player_points[i] == push_players:
@@ -162,29 +221,26 @@ def find_active_players():
 
     # If not a push then add all players with a current bank
     else:
-        print('Finding regular active players')
+        #print('Finding regular active players')
 
         for i in range(num_players):
             if player_banks[i] != 0:
                 active_players.insert(i + current_round % players_remaining, i)
-                print('Player {:1d}, round index: {:1d}'.format(i+1,i + players_remaining % (current_round+1)))
+                #print('Player {:1d}, round index: {:1d}'.format(i+1,i + players_remaining % (current_round+1)))
 
+    random.shuffle(active_players)
     #print(active_players)
     return
 
-#
-#   Set constants
-#
-game_version = .1
-num_players = 4
-push = 0
-betting_pool = 0
-player_points = []
-player_banks = []
-player_bet = []
-active_players = []
+def find_winner():
+    for i in range(num_players):
+        if player_banks[i] != 0:
+            print('Game over! Player {:1d} wins!'.format(i))
+    return
 
-
+#
+#   Begin main program
+#
 print('Cee-lo - version {:.1f}'.format(game_version))
 
 #
@@ -211,37 +267,32 @@ for i in range(num_players):
     player_banks.insert(i, 100)
     player_bet.insert(i, 0)
 
+find_active_players()
+
 #
-#   Game logic
+#   Print initial banks
 #
-#while players_remaining > 1:
-
-#   Reset loop values
-winpoint = 0
-current_round = 0
-
-
-
 print_banks(num_players)
 
+#
+#   Start game with all players
+#
 while players_remaining > 1:
     #   Show round number and remaining number of players
     print('=========\nRound {:1d}\nPlayers remainaing: {:1d}\n---------'.format(current_round+1,players_remaining))
-    print('M - Push value: {:1d}'.format(push))
-    # Find active players
-    find_active_players()
-
-    # Print players set points
-    print_setpoints(active_players)
-
 
     if betting_pool == 0:
         place_bets(active_players)
-    play_round(active_players)
+    push_round = play_round(active_players)
 
     current_round += 1
-    players_remaining = len(active_players)
-    print_setpoints(active_players)
+    #print_setpoints(active_players)
     print_banks(num_players)
     active_players = []
+
+    # Find active players
+    find_active_players(push_round)
+    players_remaining = len(active_players)
     input('Press Enter to continue to next round')
+
+find_winner()
