978393d2ca3cad6dd3289c4975823d445e2d2923
Dave Raney <raneyda.spam@gmail.com>
2018-01-11 17:21:27 -0600

    Changed the basic dice.py game to use the games/dice module

--- dice/dice.py
+++ dice/dice.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
-#
-#   This game is a roll the dice game
-#
+'''This game is a roll the dice game
+
+    Version .2 added the gamges/dice module
+'''
 
 #
 #   Imports
 #
-import random
+import sys
+sys.path.append('N:\dev\games\\')
 
-#
-#
-#
-def roll_dice():
-    dice_value = 0
-    prob = random.random()
-    # print('Random number: {:.2f}'.format(prob))
-    if prob <= .166:
-        # print('1')
-        dice_value = 1 
-    elif prob > .166 and prob <= .332:
-        # print('2')
-        dice_value = 2 
-    elif prob > .332 and prob <= .498:
-        # print('3')
-        dice_value = 3 
-    elif prob > .498 and prob <= .664:
-        # print('4')
-        dice_value = 4 
-    elif prob > .664 and prob <= .83:
-        # print('5')
-        dice_value = 5 
-    else:
-        # print('6')
-        dice_value = 6 
+import games.dice
 
-    return dice_value
-
-game_version = .1
+game_version = .2
 
 print('Dice - version {:.1f}'.format(game_version))
 
-die1, die2 = roll_dice(), roll_dice()
+die1, die2 = games.dice.roll_die(), games.dice.roll_die()
 
 print('Die 1: {:1d}, Die 2: {:1d}'.format(die1,die2))
